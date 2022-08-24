6b3e57d8e8436de8d0cb3a9671f7bad4cedf3cdd
BELLAHA Sarah (SNCF VOYAGEURS / Etablissement Gares Transilien Sud Ligne C / BTY - DOURDAN BREUILLET) <9008080s@commun.ad.sncf.fr>
2022-08-24 09:44:16 +0200

    upload exo5

--- app.js
+++ app.js
@@ -20,11 +20,13 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", validateMovie, movieHandlers.addMovie);
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", validateUser, userHandlers.addUser);
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -66,9 +66,28 @@ const updateUser = (req, res) => {
         });
 };
 
+const deleteUser = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("DELETE from `users` WHERE id=?", [id])
+        .then(([result]) => {
+            if(result.affectedRows === 0 ) {
+                res.status(404).send("Not Found");
+            } else {
+                res.status(200).send("Successfully deleted !");
+            }
+        })
+        .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error deleting the movie");
+        })
+}
+
 module.exports = {
     getUsers,
     getUserById,
     addUser,
-    updateUser
+    updateUser,
+    deleteUser
 };
